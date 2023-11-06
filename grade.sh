fe28c8f326a61214d3e5290e85413110ef42e9ca
danielburstein <danielburstein123@gmail.com>
2023-11-06 13:50:56 -0800

    added some changes

--- grade.sh
+++ grade.sh
@@ -1,4 +1,5 @@
-CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
+
+CPATH='.:../lib/hamcrest-core-1.3.jar:../lib/junit-4.13.2.jar'
 
 rm -rf student-submission
 rm -rf grading-area
@@ -7,6 +8,19 @@ mkdir grading-area
 
 git clone $1 student-submission
 echo 'Finished cloning'
+if [[ -f ./student-submission/ListExamples.java ]]
+then
+    cp -r ./TestListExamples.java ./grading-area
+    cp -r ./student-submission/ListExamples.java ./grading-area
+
+    cd grading-area/
+    javac -cp $CPATH *.java
+    java -cp $CPATH org.junit.runner.JUnitCore TestListExamples
+
+else 
+    echo 'It seems like your file is not named "ListExamples.java"'
+fi
+
 
 
 # Draw a picture/take notes on the directory structure that's set up after
